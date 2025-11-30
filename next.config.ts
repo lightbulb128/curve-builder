7f6a2ae6ff4e2c5848def6651d61ccafc774a121
lightbulb128 <1286482110@qq.com>
2025-12-01 00:48:05 +0800

    github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] || "";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // enables static export to ./out
+  trailingSlash: true,
+  assetPrefix: isGithubPages && repoName ? `/${repoName}/` : undefined,
+  basePath: isGithubPages && repoName ? `/${repoName}` : undefined,
 };
 
 export default nextConfig;
